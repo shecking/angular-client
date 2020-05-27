383c244de12ef5f08dd48613b93b082819c8df95
Steven Hecking <shecking1115@gmail.com>
2020-05-27 17:06:35 -0400

    Functional test case using 'books' for linking to Rails backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Book } from '../models/book'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [HttpClient]
 })
 export class AppComponent {
   title = 'box-the-house-client';
+  books: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:4741/books.json').subscribe(res => this.books = res);
+  }
 }
